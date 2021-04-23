d05038bf2d68bdac2c278cd06558347d7eee6b95
Radu Stoleru <radu@packaly.com>
2021-04-23 23:59:50 +0300

    feat: CLI improvements

--- cli/firegate.ts
+++ cli/firegate.ts
@@ -1,16 +1,17 @@
+import 'ts-node/register';
 import * as Commander from 'commander';
 import * as CommandLoader from './loader';
 
-void (function main(): void {
+void (function main(args: string[]): void {
     Commander.program
         .version(require('../package.json').version, '-v, --version', 'output the current version')
         .usage('<command> [options]')
         .helpOption('-h, --help', 'output usage information');
 
     CommandLoader.load(Commander);
-    Commander.program.parse(process.argv);
+    Commander.program.parse(args);
 
-    if (!process.argv.slice(2).length) {
+    if (!args.slice(2).length) {
         Commander.program.outputHelp();
     }
-})();
+})(process.argv);
